70b39242ddb7e37c6c7b57ee30ba2823ecf47e79
Diego Loya <d_l56@txstate.edu>
2017-03-24 02:40:12 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/diego/schoolapp/RegisterActivity.java
@@ -0,0 +1,31 @@
+package com.example.diego.schoolapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etName = (EditText) findViewById(R.id.etName);
+
+        final Button bRegister = (Button) findViewById(R.id.bRegister);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/diego/schoolapp/UserAreaActivity.java
@@ -0,0 +1,23 @@
+package com.example.diego.schoolapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class UserAreaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_area);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final TextView welcomeMessage = (TextView) findViewById(R.id.tvWelcomeMessage);
+
+
+
+
+    }
+}
